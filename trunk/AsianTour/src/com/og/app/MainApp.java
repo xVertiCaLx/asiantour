2b215e3d7d4559a388f55d0a541544cb9fccafc3
aloysius.ong@hotmail.sg <aloysius.ong@hotmail.sg@7ed951c8-8523-f870-0e13-948aae4dd89d>
2010-07-14 08:48:24 +0000

    test commit

--- trunk/AsianTour/src/com/og/app/MainApp.java
+++ trunk/AsianTour/src/com/og/app/MainApp.java
@@ -19,5 +19,6 @@ public class MainApp extends UiApplication{
         SplashScreen s = new SplashScreen();
         pushScreen(s);
         enterEventDispatcher();
+        //added line test
     }
 } 
