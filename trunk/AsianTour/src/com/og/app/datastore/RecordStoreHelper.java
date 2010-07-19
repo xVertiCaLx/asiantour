d6d6d8bebc8e0c6475e16f6f0da7535d6e9c3bb3
lotified <lotified@7ed951c8-8523-f870-0e13-948aae4dd89d>
2010-07-19 08:19:59 +0000

    Added helper methods to facilitate retrieving,setting and counting news item in recordstore

--- trunk/AsianTour/src/com/og/app/datastore/RecordStoreHelper.java
+++ trunk/AsianTour/src/com/og/app/datastore/RecordStoreHelper.java
@@ -9,11 +9,11 @@ import com.og.rss.ANewsItemObj;
 
 public class RecordStoreHelper {
 	public static final long DATASTORE_KEY = 0x8d0ed7fadc56615cL;
-	
+
 	private static PersistentObject getRecordStore(){
 		return PersistentStore.getPersistentObject(DATASTORE_KEY);
 	}
-	
+
 	//Vector<ANewsItemObj>
 	public static Vector getNewsCollection(){
 		PersistentObject recordStore = getRecordStore();
@@ -25,4 +25,32 @@ public class RecordStoreHelper {
 		}
 		return newsCollection;
 	}
+
+	//Vector<ANewsItemObj>
+	public static void setNewsCollection(Vector aNewsItemObj){
+		PersistentObject recordStore = getRecordStore();
+		recordStore.setContents(aNewsItemObj);
+		recordStore.commit();
+	}
+
+	public static int getNewsCount(){
+		return getNewsCollection().size();
+	}
+
+	public static boolean isNewsRead(int newsID){
+		int newsCount = getNewsCount();
+		if(newsCount==0){
+			return false;
+		}
+		Vector newsCollection = getNewsCollection();
+		for(int i=0 ;i< newsCount; i++){
+			ANewsItemObj news = (ANewsItemObj)newsCollection.elementAt(i);
+			if(newsID == news.index){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
 }
