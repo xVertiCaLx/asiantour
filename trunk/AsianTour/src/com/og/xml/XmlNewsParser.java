d9956ced5eed8363e64e500508421ba6bf67af53
lotified <lotified@7ed951c8-8523-f870-0e13-948aae4dd89d>
2010-07-20 04:09:35 +0000

    Finished news xml parser.

---
+++ trunk/AsianTour/src/com/og/xml/XmlNewsParser.java
@@ -0,0 +1,91 @@
+package com.og.xml;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Vector;
+
+import net.rim.device.api.util.StringUtilities;
+import net.rim.device.api.xml.parsers.DocumentBuilder;
+import net.rim.device.api.xml.parsers.DocumentBuilderFactory;
+import net.rim.device.api.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlNewsParser {
+	
+	public static XmlNewsItem parse(String strToParse){
+		XmlNewsItem item = null;
+		byte[] xmlByteArray = strToParse.getBytes();
+		ByteArrayInputStream xmlStream = new ByteArrayInputStream(xmlByteArray);
+		InputStreamReader xmlReader = new InputStreamReader(xmlStream);
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory. newInstance(); 
+		try {
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(xmlStream);
+			doc.getDocumentElement().normalize();
+			NodeList list = doc.getElementsByTagName("*");
+			String node = "";
+			String element = "";
+			Vector xmlNewsItemCollection = new Vector();
+			XmlNewsItem xmlNewsItem = new XmlNewsItem();
+			for(int i=0; i<list.getLength(); i++){
+				Node value = list.item(i).getChildNodes().item(0);
+				node = list.item(i).getNodeName();
+				element = value.getNodeValue();
+				System.out.println(i + ": Node["+node+"], Element["+element+"]");
+				
+				if(node.equals("item") && i!=1){
+					xmlNewsItemCollection.addElement(xmlNewsItem);
+					xmlNewsItem = new XmlNewsItem();
+				}else if(node.equals("link")){
+					String id = element.substring("http://www.asiantour.com/news.aspx?sid=".length());
+					xmlNewsItem.id = id;
+				}else if(node.equals("title")){
+					xmlNewsItem.title = element;
+				}else if(node.equals("description")){
+					xmlNewsItem.description = element;
+				}else if(node.equals("thumbnail")){
+					xmlNewsItem.thumbnailURL = element;
+				}else if(node.equals("pubDate")){
+					String dateString = element;
+					String gmt = "GMT" + dateString.substring(dateString.indexOf(" +"));
+					Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(gmt));
+					String [] dateWords = StringUtilities.stringToWords(dateString);
+					String[] MONTHS = { "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec" };
+					int month = 0;
+					for(int j=0; j<MONTHS.length;j++){
+						if(MONTHS[j].equals(dateWords[2])){
+							month = j;
+							break;
+						}
+					}
+					calendar.set(Calendar.DATE, Integer.parseInt(dateWords[1]));
+					calendar.set(Calendar.MONTH, month);
+					calendar.set(Calendar.YEAR, Integer.parseInt(dateWords[3]));
+					calendar.set(Calendar.HOUR, Integer.parseInt(dateWords[4]));
+					calendar.set(Calendar.MINUTE, Integer.parseInt(dateWords[5]));
+					calendar.set(Calendar.SECOND, Integer.parseInt(dateWords[6]));
+					xmlNewsItem.pubDate = calendar;
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return item;
+	}
+}
