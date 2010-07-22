e1a453e1f876b1d6312a86c65ac26c821f0b40c5
lotified <lotified@7ed951c8-8523-f870-0e13-948aae4dd89d>
2010-07-22 07:07:46 +0000

    Will now cache images downloaded from NewsDetailScreen

--- trunk/AsianTour/src/com/og/app/gui/component/WebBitmapField.java
+++ trunk/AsianTour/src/com/og/app/gui/component/WebBitmapField.java
@@ -4,18 +4,21 @@ import net.rim.device.api.system.Bitmap;
 import net.rim.device.api.system.EncodedImage;
 import net.rim.device.api.ui.component.BitmapField;
 
+import com.og.app.gui.MenuScreen;
 import com.og.app.util.Utility;
 import com.og.app.util.WebDataCallback;
+import com.og.rss.ANewsItemObj;
 
 public class WebBitmapField extends BitmapField implements WebDataCallback
 {
 	private EncodedImage bitmap = null;
-
-	public WebBitmapField(String url)
+	private String guid = null;
+	public WebBitmapField(String url, String newsItemGuid)
 	{
 		try
 		{
 			Utility.getWebData(url, this);
+			this.guid = newsItemGuid;
 		}
 		catch (Exception e) {}
 	}
@@ -35,6 +38,13 @@ public class WebBitmapField extends BitmapField implements WebDataCallback
 			byte[] dataArray = data.getBytes();
 			bitmap = EncodedImage.createEncodedImage(dataArray, 0,
 					dataArray.length);
+			for(int i=0; i<MenuScreen.getInstance().newsCollection.size(); i++){
+				ANewsItemObj newsItem = (ANewsItemObj)MenuScreen.getInstance().newsCollection.elementAt(i);
+				if(newsItem.guid.equals(guid)){
+					newsItem.image = dataArray;
+					break;
+				}
+			}
 			setImage(bitmap);
 		}
 		catch (final Exception e){}
