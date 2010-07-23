594c1cf4c6074e6147df277552baafe8e2b752ef
lotified <lotified@7ed951c8-8523-f870-0e13-948aae4dd89d>
2010-07-23 04:10:16 +0000

    Fixed Tab Menu

--- AsianTour/src/com/og/app/gui/MenuScreen.java
+++ AsianTour/src/com/og/app/gui/MenuScreen.java
@@ -15,140 +15,135 @@ import com.og.app.gui.listener.TabListener;
 import com.og.xml.XmlHelper;
 
 public class MenuScreen extends MainScreen implements TabListener, ListFieldListener, FocusChangeListener {
-    
+
 	//Vector<ANewsItemObj>
 	public Vector newsCollection = new Vector();
-	
-    public static MenuScreen thisInstance = null;
-    
-    private int selectedTab = 1;
-    private int prevSelectedTab = 0;
-    private boolean isTabFocus = true;
-    
-    private LogoPanel logoPanel = null;
-    private TabPanel tabPanel = null;
+
+	public static MenuScreen thisInstance = null;
+
+	private int selectedTab = 1;
+	private int prevSelectedTab = 0;
+	private boolean isTabFocus = true;
+
+	private LogoPanel logoPanel = null;
+	private TabPanel tabPanel = null;
 
 	public NewsPanel newsPanel = null;
-    private TablePanel tablePanel = null;
-    
-    private boolean isFocusEventBlocked = false;
-    //--------------------------- Single Turn Controll --------------------------------
-    public synchronized static MenuScreen getInstance() {
-        if (thisInstance == null) { 
-            thisInstance = new MenuScreen();
-        }
-        return thisInstance;
-    }   
-    
-    public MenuScreen() {
-        super();
-        thisInstance = this;
-        GuiConst.reinitFont();
-        fieldInit(1);
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Display height is: " + Display.getHeight() + "TABPANEL: " + GuiConst.TABPANEL_HEIGHT + "LOGOPANEL: " + GuiConst.LOGOPANEL_HEIGHT); 
-        System.out.println("-----------------------------------------------------");
-        add(logoPanel);
-        add(tabPanel);
-        add(newsPanel);
-        //LabelField lblTitle = new LabelField("Asian Tour", LabelField.ELLIPSIS | LabelField.USE_ALL_WIDTH);
-        //setTitle(lblTitle);
-        newsCollection = RecordStoreHelper.getNewsCollection();
-        XmlHelper.downloadNews();
-    }
-    
-    public void fieldInit(int tableNo) {
-    	
-    	System.out.println("-----------------------------------------------------");
-        System.out.println("Display tableNo is: " + tableNo); 
-        System.out.println("-----------------------------------------------------");
-    	
-        logoPanel = LogoPanel.getInstance();
-        tabPanel = TabPanel.getInstance(this);
-        
-        GuiConst.LOGOPANEL_HEIGHT=logoPanel.getPreferredHeight();
-        GuiConst.TABPANEL_HEIGHT=tabPanel.getPreferredHeight();
-        
-        newsPanel = NewsPanel.getInstance(this, Display.getHeight()-GuiConst.TABPANEL_HEIGHT-GuiConst.LOGOPANEL_HEIGHT);
-        tablePanel = TablePanel.getInstance(this, Display.getHeight()-GuiConst.TABPANEL_HEIGHT-GuiConst.LOGOPANEL_HEIGHT, tableNo,1);
-        
-    }
-    
-    public boolean onClose() {
-        if(Dialog.ask(Dialog.D_YES_NO, "Do you want to exit?") == Dialog.YES)
-        {
-            System.out.println("aloy.endapp");
-            RecordStoreHelper.setNewsCollection(newsCollection);
-            clearResource();
-            System.exit(0);
-        }
-        return true;
-    }
-    
-    public void clearResource() { 
-    	
-        tabPanel = null;        
-        logoPanel = null;
-        newsPanel = null;
-        tablePanel = null;
-        
-    }
-    
-    public void repainteverything() {
-    	this.deleteAll();
-    	tablePanel.reinitThisThing();
-    }
-    
-    public void repaintTab(){
-        tabPanel.repaintTab();
-    }
-    
-    public int getSelectedTab(){
-        return selectedTab;
-    }
-    
-    public void setSelectedTab(int tabID) {
-    	for(int i=0; i<getFieldCount(); i++){
-    		Field f = getField(i);
-    		if(f instanceof TabField){
-    			TabField tf = (TabField)f;
-    			if(tabID == tf.getTabID()){
-    				tf.setFocus();
-    				selectedTab = tabID;
-    				return;
-    			}
-    		}
-    	}
-    }
-    
-    public void setTabOnFocus(boolean isFocus){
-         isTabFocus = isFocus;
-    }    
-    public boolean isTabOnFocus(){
-         return isTabFocus;
-    }
-    
-    public void reinitTab(){
-        tabPanel.reinitTab();
-        tabPanel.setTabFocus(selectedTab);
-    }
-    
-    public void repaintScreen(){
-        //this.removeAllMenuItems();
-        //addMenuItems();
-        
-        logoPanel.invalidate();
-        setSelectedTab(9);//this.selectedTab);
-        tabPanel.reinitTab();      
-        
-        if (this.selectedTab == 9) {
-            setSelectedTab(9);
-        } else if (this.selectedTab == 10) {
-            setSelectedTab(10);
-        }
-        
-        //newsPanel.loadNews(selectedTab);          
-        /*        
+	private TablePanel tablePanel = null;
+
+	private boolean isFocusEventBlocked = false;
+	//--------------------------- Single Turn Controll --------------------------------
+	public synchronized static MenuScreen getInstance() {
+		if (thisInstance == null) { 
+			thisInstance = new MenuScreen();
+		}
+		return thisInstance;
+	}   
+
+	public MenuScreen() {
+		super();
+		thisInstance = this;
+		GuiConst.reinitFont();
+		fieldInit(1);
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Display height is: " + Display.getHeight() + "TABPANEL: " + GuiConst.TABPANEL_HEIGHT + "LOGOPANEL: " + GuiConst.LOGOPANEL_HEIGHT); 
+		System.out.println("-----------------------------------------------------");
+		add(logoPanel);
+		add(tabPanel);
+		add(newsPanel);
+		//LabelField lblTitle = new LabelField("Asian Tour", LabelField.ELLIPSIS | LabelField.USE_ALL_WIDTH);
+		//setTitle(lblTitle);
+		newsCollection = RecordStoreHelper.getNewsCollection();
+		XmlHelper.downloadNews();
+	}
+
+	public void fieldInit(int tableNo) {
+		logoPanel = LogoPanel.getInstance();
+		tabPanel = TabPanel.getInstance(this);
+
+		GuiConst.LOGOPANEL_HEIGHT=logoPanel.getPreferredHeight();
+		GuiConst.TABPANEL_HEIGHT=tabPanel.getPreferredHeight();
+
+		newsPanel = NewsPanel.getInstance(this, Display.getHeight()-GuiConst.TABPANEL_HEIGHT-GuiConst.LOGOPANEL_HEIGHT);
+		tablePanel = TablePanel.getInstance(this, Display.getHeight()-GuiConst.TABPANEL_HEIGHT-GuiConst.LOGOPANEL_HEIGHT, tableNo,1);
+
+	}
+
+	public boolean onClose() {
+		if(Dialog.ask(Dialog.D_YES_NO, "Do you want to exit?") == Dialog.YES)
+		{
+			System.out.println("aloy.endapp");
+			RecordStoreHelper.setNewsCollection(newsCollection);
+			clearResource();
+			System.exit(0);
+		}
+		return true;
+	}
+
+	public void clearResource() { 
+
+		tabPanel = null;        
+		logoPanel = null;
+		newsPanel = null;
+		tablePanel = null;
+
+	}
+
+	public void repainteverything() {
+		this.deleteAll();
+		tablePanel.reinitThisThing();
+	}
+
+	public void repaintTab(){
+		tabPanel.repaintTab();
+	}
+
+	public int getSelectedTab(){
+		return selectedTab;
+	}
+
+	public void setSelectedTab(int tabID) {
+		for(int i=0; i<getFieldCount(); i++){
+			Field f = getField(i);
+			if(f instanceof TabField){
+				TabField tf = (TabField)f;
+				if(tabID == tf.getTabID()){
+					tf.setFocus();
+					selectedTab = tabID;
+					return;
+				}
+			}
+		}
+	}
+
+	public void setTabOnFocus(boolean isFocus){
+		isTabFocus = isFocus;
+	}    
+	public boolean isTabOnFocus(){
+		return isTabFocus;
+	}
+
+	public void reinitTab(){
+		tabPanel.reinitTab();
+		tabPanel.setTabFocus(selectedTab);
+	}
+
+	public void repaintScreen(){
+		//this.removeAllMenuItems();
+		//addMenuItems();
+
+		logoPanel.invalidate();
+		setSelectedTab(9);//this.selectedTab);
+		tabPanel.reinitTab();      
+
+		if (this.selectedTab == 9) {
+			setSelectedTab(9);
+		} else if (this.selectedTab == 10) {
+			setSelectedTab(10);
+		}
+
+		//newsPanel.loadNews(selectedTab);          
+		/*        
         if ( this.selectedTab==Const.ID_SETTING ){
             delete(newscategorypanel);
             newscategorypanel = new NewsCategoryPanel(rssdb, newspanel.getPreferredHeight(),this);
@@ -157,42 +152,42 @@ public class MenuScreen extends MainScreen implements TabListener, ListFieldList
             setSelectedTab(Const.ID_PHOTOGALLERY );
         }else
             newsPanel.loadNews(selectedTab);
-        */
-    }
-    
-    public void drawBottomPanel(){
-    	
-            /*if ( selectedTab==9)
+		 */
+	}
+
+	public void drawBottomPanel(){
+
+		/*if ( selectedTab==9)
                 //photopanel.loadPhoto(selectedTab);    
                 newsPanel.loadNews(selectedTab);          
             else if ( selectedTab==10 )
                 newsPanel.loadNews(selectedTab);  */      
-    }
-    
-    public boolean isSelectedTabHasNews(){
-        /*if ( selectedTab==Const.ID_SETTING){
+	}
+
+	public boolean isSelectedTabHasNews(){
+		/*if ( selectedTab==Const.ID_SETTING){
                 return true;    
         }else if ( selectedTab==Const.ID_PHOTOGALLERY){
                return photopanel.hasPhoto();     
         }    
         return newspanel.hasNews();*/
-        return false;
-    }
-    
-    
-    //-----------------------------------------------------------------------------------------------------    
-//START -- method for ListFieldListener
-    public void onListFieldUnfocus(){
-        //System.out.println("onListFieldUnfocus:tabSelected:"+selectedTab);
-        tabPanel.setTabFocus(selectedTab);
-        isTabFocus=true;
-    }
-    public boolean isListFieldFocus(){
-        if ( isTabFocus )
-            return false;
-        return true;
-    }     
-//END -- method for ListFieldListener
+		return false;
+	}
+
+
+	//-----------------------------------------------------------------------------------------------------    
+	//START -- method for ListFieldListener
+	public void onListFieldUnfocus(){
+		//System.out.println("onListFieldUnfocus:tabSelected:"+selectedTab);
+		tabPanel.setTabFocus(selectedTab);
+		isTabFocus=true;
+	}
+	public boolean isListFieldFocus(){
+		if ( isTabFocus )
+			return false;
+		return true;
+	}     
+	//END -- method for ListFieldListener
 
 	public void focusChanged(Field field, int eventType) {
 		if(isFocusEventBlocked){
@@ -200,45 +195,57 @@ public class MenuScreen extends MainScreen implements TabListener, ListFieldList
 		}
 		if(field instanceof TabField){
 			TabField f = (TabField)field;
-				if(eventType == FOCUS_LOST){
-					prevSelectedTab = f.getTabID();
-					System.out.println("Tab " + f.getTabID() + " is out of focus!, Selected Tab: " + selectedTab);
-					if(f.getTabID()==selectedTab){
+			int currTabId = f.getTabID();
+			if(eventType == FOCUS_LOST){
+				System.out.println("TAB_FOCUS_LOST: " + currTabId);
+				if(currTabId==TabPanel.TAB_ORDER_OF_MERIT){
+					if(selectedTab==TabPanel.TAB_TOUR_SCHEDULE){
+						prevSelectedTab = TabPanel.TAB_ORDER_OF_MERIT;
+					}
+					else if(selectedTab!=TabPanel.TAB_ORDER_OF_MERIT){
 						return;
 					}
-					
 				}
-				else{
-					if(f.getTabID()==selectedTab){
-						return;
-					}
-					if(f.getTabID() == TabPanel.TAB_ORDER_OF_MERIT && selectedTab == prevSelectedTab){
-						setSelectedTab(selectedTab);
-						return;
-					}
-					System.out.println("Tab " + f.getTabID() + " is in focus! , Selected Tab: " + selectedTab);
-					selectedTab = f.getTabID();
-					switch(selectedTab){
-						case TabPanel.TAB_NEWS:
-							showNewsTab();
-							break;
-						case TabPanel.TAB_LIVE_SCORE:
-							setSelectedTab(TabPanel.TAB_LIVE_SCORE);
-							break;
-						case TabPanel.TAB_TV_SCHEDULE:
+				prevSelectedTab = currTabId;
+			}
+			else{
+				System.out.println("TAB_FOCUS_GAINED: " + currTabId);
+				switch(currTabId){
+				case TabPanel.TAB_NEWS:
+					showNewsTab();
+					break;
+				case TabPanel.TAB_LIVE_SCORE:
+					setSelectedTab(currTabId);
+					break;
+				case TabPanel.TAB_TV_SCHEDULE:
+					showTVScheduleTab();
+					break;
+				case TabPanel.TAB_TOUR_SCHEDULE:
+					showTourScheduleTab();
+					break;
+				case TabPanel.TAB_ORDER_OF_MERIT:
+					if(prevSelectedTab!=TabPanel.TAB_ORDER_OF_MERIT){
+						System.out.println("Jumping back to : " + prevSelectedTab);
+						if(prevSelectedTab == TabPanel.TAB_TV_SCHEDULE){
 							showTVScheduleTab();
-							break;
-						case TabPanel.TAB_TOUR_SCHEDULE:
+							return;
+						}
+						if(prevSelectedTab == TabPanel.TAB_TOUR_SCHEDULE){
 							showTourScheduleTab();
-							break;
-						case TabPanel.TAB_ORDER_OF_MERIT:
-							setSelectedTab(TabPanel.TAB_ORDER_OF_MERIT);
-							break;
+							return;
+						}
+						if(prevSelectedTab==TabPanel.TAB_NEWS){
+							return;
+						}
 					}
-				}					
+					setSelectedTab(currTabId);
+					break;
+				}
+				selectedTab = f.getTabID();
+			}					
 		}		
 	}
-	
+
 	private void showNewsTab(){
 		repainteverything();
 		fieldInit(1);
@@ -246,51 +253,50 @@ public class MenuScreen extends MainScreen implements TabListener, ListFieldList
 		NewsPanel.newsPanel.newsList.setSize(newsCollection.size());
 		isFocusEventBlocked = true;
 		add(logoPanel);
-        add(tabPanel);
-        add(newsPanel);
-        setSelectedTab(TabPanel.TAB_NEWS);
-        isFocusEventBlocked = false;
-        System.out.println("newsCollection has " + newsCollection.size() + " news.");
+		add(tabPanel);
+		add(newsPanel);
+		setSelectedTab(TabPanel.TAB_NEWS);
+		isFocusEventBlocked = false;
 	}
-    
+
 	private void showTVScheduleTab(){
-		
+
 		repainteverything();
 		fieldInit(1);
 		isFocusEventBlocked = true;
 		add(logoPanel);
-        add(tabPanel);
-        add(tablePanel);
-        setSelectedTab(TabPanel.TAB_TV_SCHEDULE);
-        isFocusEventBlocked = false;
+		add(tabPanel);
+		add(tablePanel);
+		setSelectedTab(TabPanel.TAB_TV_SCHEDULE);
+		isFocusEventBlocked = false;
 	}
-	
+
 	private void showTourScheduleTab(){
 		repainteverything();
 		fieldInit(2);
 		isFocusEventBlocked = true;
 		add(logoPanel);
-        add(tabPanel);
-        add(tablePanel);
-        setSelectedTab(TabPanel.TAB_TOUR_SCHEDULE);
-        isFocusEventBlocked = false;
+		add(tabPanel);
+		add(tablePanel);
+		setSelectedTab(TabPanel.TAB_TOUR_SCHEDULE);
+		isFocusEventBlocked = false;
 	}
-    
-    /*--------------------------- Example of making a Horizontal Field Manager ----------------
-    
+
+	/*--------------------------- Example of making a Horizontal Field Manager ----------------
+
         //--------------------------- Panel Manager Initialization ----------------------------
         HorizontalFieldManager tabPanel = new HorizontalFieldManager();
-        
+
         //--------------------------- Adding it to the screen ---------------------------------
         add(tabPanel);
-        
+
         //--------------------------- Horizontal Tab Manager ----------------------------------
         BitmapField TAB_BG_LEFT = new BitmapField(selectedTabLeft);
         BitmapField TAB_BG_MIDDLE = new BitmapField(selectedTabMiddle, BitmapField.ELLIPSIS |  BitmapField.USE_ALL_WIDTH);
         BitmapField TAB_BG_RIGHT = new BitmapField(selectedTabRight);
-        
+
         tabPanel.add(TAB_BG_LEFT);
         tabPanel.add(TAB_BG_RIGHT);
-        
+
         --------------------------- End of Horizontal Field Manager Example -------------------*/
 } 
